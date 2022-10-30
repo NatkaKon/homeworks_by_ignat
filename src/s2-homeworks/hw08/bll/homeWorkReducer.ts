859be15314ca7bb0cb2b73c19f8e5c806e763a65
Natalia Kondratieva <kondratieva07@gmail.com>
2022-10-30 16:58:52 +0300

    task hw08 correct

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -19,7 +19,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
         }
         case 'check': {
 
-            return state.filter(f => f.age > action.payload) // need to fix
+            return state.filter(f => f.age >= action.payload) // need to fix
         }
         default:
             return state
